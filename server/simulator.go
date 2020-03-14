15d8489984ae6c867edb5726eba0818ccf4bd039
Julen Dixneuf <julen@student.ecp.fr>
2020-03-14 10:52:44 +0100

    use same simulator as ours

--- server/simulator.go
+++ server/simulator.go
@@ -48,33 +48,18 @@ func simulateHumanFight(E1, E2 int) (survivor int, victory bool) {
 
 // getProba reimplements the getProba logic from Board.cs in the C# implementation
 func getProba(E1, E2 int, involveHumans bool) float64 {
-	if E1 == E2 {
-		return 0.5
-	}
-	var cste float64
-	if involveHumans {
-		cste = 1
-	} else {
-		cste = 1.5
-	}
-
 	// True by property
-	if float64(E1) >= cste*float64(E2) {
+	if (involveHumans && E1 >= E2) || (!involveHumans && float64(E1) >= 1.5*float64(E2)) {
 		return 1
 	}
 
-	var x0, y0 float64
-	x1 := float64(E2)
-	y1 := 0.5
+	if E1 == E2 {
+		return 0.5
+	}
+
 	if E1 < E2 {
-		x0 = 0
-		y0 = 0
-		return (y0 - y1) / (x0 - x1) * float64(E1)
+		return float64(E1) / (2 * float64(E2))
 	} else {
-		x0 = cste * float64(E2)
-		y0 = 1
-		m := (y0 - y1) / (x0 - x1)
-		c := 1 - m*x0
-		return m*float64(E2) + c
+		return (float64(E1) / float64(E2)) - 0.5
 	}
 }
